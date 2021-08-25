101aab862eda5815527b15634c89dd2a80d0c76f
Oueslatiwael <Oueslatiwael17@gmail.com>
2021-08-25 11:01:46 +0100

    react commit

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import wejhi from './Image.jpg'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={wejhi} />
+        </div>
+    )
+}
+
+export default ProfilePhoto
